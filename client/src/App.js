ccc5aaf6fd416b09937d31c6fac973b981367cbc
JonathanSC <jonathansolisc_22@hotmail.com>
2018-07-07 13:09:02 -0500

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import loginWithFacebook from './img/loginWithFacebook.png';
 import Auth from '../src/utils/autho';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/AppNavbar.js';
 
 const auth = new Auth();
-
 
 class LogIn extends Component {
   constructor(){
@@ -28,11 +29,13 @@ class LogIn extends Component {
   render() {
     return (
       <div className="App">
+      <AppNavbar />
       <h1>Hello</h1>
       <img src={loginWithFacebook} alt="Login with facebook button" onClick={this.handleClick}/>
       {/* <button onClick={this.handleClick}>login with Facebook</button> */}
         {/* <button onClick={this.handleAuthentication}>Aunthentication with Facebook</button>
         <button onClick={this.getInfoUser}>get user's info</button> */}
+        <i class="fab fa-github"></i>
       </div>
     );
   }
