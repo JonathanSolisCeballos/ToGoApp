b5f4acf0d721e22c89025fc108c6a3603c1e6c4c
JacquelineLK119 <jflores43@ucol.mx>
2018-07-08 15:59:45 -0500

    Menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import prueba from './vjs.png';
 import './App.css';
+import { todos } from './bt.json';
 
-import { todos } from './todos.json';
+import bmM from './menu/big-mac_Mcds.png';
+import cafeM from './menu/cafe_Mcds.jpg';
+import ccM from './menu/coca-cola_Mcds.png';
+import hdcqM from './menu/hamb-doble-con-queso_Mcds.png';
+import mcfoM from './menu/mc-flurry-oreo_Mcds.png';
+import mcpsoM from './menu/mc-patatas_Mcds.png';
+import mcpM from './menu/mc-pollo_Mcds.png';
+import palfM from './menu/papas-a-la-francesa_Mcds.png';
+import payM from './menu/pay_Mcds.png';
 
-import TodoForm from './components/TodoForm';
+import bt from './menu/banana-tempura.png';
+import crepa from './menu/crepa.png';
+import gyoza from './menu/gyoza.jpg';
+import msh from './menu/misoshiro.jpg';
+import pj from './menu/paella-japonesa.jpg';
+import sun from './menu/sunomono.jpg';
+import sushi from './menu/sushi.jpg';
+import tyk from './menu/teriyaki.jpg';
+import tkts from './menu/torikatsu.jpg';
+
+const productosm= [hdcqM, cafeM, bmM, payM, mcfoM, ccM, mcpsoM, palfM, mcpM];
+const productosbt= [bt, sun, pj, tkts, tyk, sushi, crepa, gyoza, msh];
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            todos
+            todos,
+            product: '',
+            company: '',
+            description: '',
+            price: '',
+            total: ''
+        };
+          this.handleAddTodo = this.handleAddTodo.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+          this.countTotal = this.countTotal.bind(this);
+    }
+    
+    removeTodo(index) {
+      var total1=parseInt(this.state.todos[index].total);
+
+        if(total1>0){
+            this.state.todos[index].total=total1-1;
+         }else{
+            this.state.todos[index].total=0;
         }
-        this.handleAddTodo = this.handleAddTodo.bind(this);
     }
 
-    removeTodo(index) {
-        this.setState({
-            todos: this.state.todos.filter((e, i) => {
-                return i !== index
-            })
-        });
-    }
+    handleSubmit(e) {
+      e.preventDefault();
+      this.setState({
+        product: '',
+        company: '',
+        description: '',
+        price: '',
+        total: ''
+      });
+  }
+
+    addFood(index) {
+      var total1=parseInt(this.state.todos[index].total);
+        if(total1<10){
+            this.state.todos[index].total=total1+1;
+          }else if(total1==10){
+            this.state.todos[index].total=10;
+        }
+     }
 
     handleAddTodo(todo) {
         this.setState({
@@ -30,84 +77,60 @@ class App extends Component {
         })
     }
 
+    countTotal(){
+      var index=0;
+      var ct=[];
+      var cn=[];
+      var cp=[];
+
+      for(var i=0; i<this.state.todos.length; i++){
+        if(this.state.todos[i].total>0){
+          console.log(this.state.todos[i].product);
+
+          ct.push(this.state.todos[i].total);
+          cn.push(this.state.todos[i].product);
+          cp.push(this.state.todos[i].price);
+        }
+      }
+
+    }
+
     render() {
-        const todos = this.state.todos.map((todo, i) => {
-            return ( 
-                <div className = "col-md-5" key = { i } >
-                <div className = "card mt-4" >
-                <div className = "card-title text-center" >
-                <h3> { todo.title } </h3> 
-                <span className = "badge badge-pill badge-danger ml-2"> { todo.priority } </span> 
+      const todos = this.state.todos.map((todo, i) => {
+          return ( 
+              <div className = "col-md-5" key = { i } >
+           {/* Prueba del menú */}
+                <form onSubmit = { this.handleSubmit } className = "card-body" >
+                  <div className="thumbnail" key = { i } >
+                    <img src= { productosbt[i] } alt="..."/>
+                    <div className="caption">
+                      <h3>{ todo.product }</h3>
+                      <p> { todo.description } </p>
+                      <p> $ { todo.price } </p>
+                      <p><button type="submit" className = "btn btn-danger" onClick = { this.removeTodo.bind(this, i)} > - </button> 
+                      <span className = "badge" > { this.state.todos[i].total } </span>  
+                      <button className = "btn btn-success" onClick = { this.addFood.bind(this, i) }> + </button></p>
+                    </div>
+                  </div> 
+                 </form > 
                 </div>
-                <div className = "card-body" > { todo.description } </div> 
-                <div className = "card-footer" >
-                <button className = "btn btn-danger" onClick = { this.removeTodo.bind(this, i) }> Delete </button> 
-                </div>
-                </div> 
-                <div class="col-sm-6 col-md-4">
-
-    <div className="thumbnail" key = { i } >
-      <img src={prueba} alt="..."/>
-      <div className="caption">
-        <h3>{ todo.title }</h3>
-        <p> { todo.priority } </p>
-        <p> { todo.description } </p>
-        <p><button className = "btn btn-danger" onClick = { this.removeTodo.bind(this, i) }> - </button>  
-        <a href="#" classNAme="btn btn-default" role="button"> + </a></p>
-      </div>
-    </div>
-  </div>
-
-
-                </div>
-                
             )
         });
 
-        // RETURN THE COMPONENT
         return ( 
             <div className = "App" >
-            <nav className = "navbar navbar-dark bg-dark" >
-            <a className = "navbar-brand" href = "/" > Tasks 
-            <span className = "badge badge-pill badge-light ml-2" > { this.state.todos.length } 
-            </span> 
-            </a > 
-            </nav>
-
-{/* Menú */}
-            <nav className = "navbar navbar-dark" >
-              <a className = "navbar-brand" href = "#ihhj" > ToGo </a> 
-                <ul className="nav justify-content-end">
-                    <li className="nav-item">
-                      <a className="nav-link" href="#qs">¿QUIÉNES SOMOS?</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#contacto">CONTACTO</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#jh">INICIAR SESIÓN</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#hv">REGISTRARSE</a>
-                    </li>
-                </ul>
-            </nav>
-
-      {/*  */}
-
-            <div className = "container" >
-            <div className = "row mt-4" >
-            <div className = "col-md-4 text-center" >
-            <img src = { logo } className = "App-logo" alt = "logo" />
-            <TodoForm onAddTodo = { this.handleAddTodo } > </TodoForm> </div>
-            <div className = "col-md-8" >
-            <div className = "row" > { todos } </div> 
-            </div > 
-            </div> 
-            </div > 
+          {/* Nav */}
+              <nav className = "navbar" >
+                <a className = "navbar-brand" href = "#ihhj" > ToGo </a> 
+              </nav>
+          {/*  */}
+              <div className = "container" >
+              <div className = "col-md-8 text-center" >
+              <div className = "row" > { todos } </div> 
+              </div></div> 
+  <button type="button" class="btn btn-default" onClick={ this.countTotal }>Resumen de compra</button>
             </div>
         );
     }
 }
-
 export default App;
